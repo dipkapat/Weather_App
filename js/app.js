9a3fd02657f1c6ae138d060739272145babe076c
Dip Kumar Kapat <kapat.dip@gmail.com>
2017-08-22 22:29:37 +0530

    Update
    Final Update

--- js/app.js
+++ js/app.js
@@ -1,73 +1,83 @@
-'use strict';
+function display() {
+    var zip = document.getElementById('zip').value;
+    var code = document.getElementById('countryCode').value;
+    var weatherConditions = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "," + code + "&units=metric&APPID=0dfa7081b88c8bb4ce043507ed280eb2&cnt=3&callback";
+    var weatherForecast = "http://api.openweathermap.org/data/2.5/forecast?zip=" + zip + "," + code + "&units=metric&APPID=0dfa7081b88c8bb4ce043507ed280eb2&cnt=3&callback";
+    var baseImgPath = "http://openweathermap.org/img/w/";
 
-var weatherConditions = new XMLHttpRequest();
-var weatherForecast = new XMLHttpRequest();
-var conditionObj;
-var forecastObj;
+    $.getJSON(weatherConditions, function(condition) {
+        console.log(condition);
+        if (condition.cod === 200) {
+            document.getElementById('location').innerHTML = condition.name + ", " + condition.sys.country;
+            document.getElementById('weather').innerHTML = condition.weather["0"].main;
+            document.getElementById('temperature').innerHTML = condition.main.temp + "&deg;C";
+            var imagePath = baseImgPath + condition.weather["0"].icon + ".png";
+            document.getElementById('weatherIcon').src = imagePath;
+            var weatherDescStr = condition.weather["0"].description;
+            document.getElementById('desc').innerHTML = weatherDescStr.substr(0, 1).toUpperCase() + weatherDescStr.substr(1);
+            document.getElementById('humidity').innerHTML = "Humidity: " + condition.main.humidity + "%";
+            var wind = parseFloat(condition.wind.speed * (18 / 5));
+            document.getElementById('wind').innerHTML = "Wind Speed: " + wind.toFixed(2) + " kmph";
+            document.getElementById('clouds').innerHTML = "Cloud Cover: " + condition.clouds.all + "%";
+            document.getElementById('coData').innerHTML = "Message: " + condition.sys.message;
+        }
+    });
+    $.getJSON(weatherForecast, function(forecast) {
+        //console.log(forecast);
+        if (forecast.cod === "200") {
+            // Day1
+            document.getElementById('dayDate1').innerHTML = aryDates[1];
+            document.getElementById('maxTemp1').innerHTML = forecast.list["0"].main.temp_max + "&deg;C";
+            document.getElementById('minTemp1').innerHTML = forecast.list["0"].main.temp_min + "&deg;C";
+            document.getElementById('dailyForecast1').innerHTML = forecast.list["0"].weather["0"].description;
+            var imagePath = baseImgPath + forecast.list["0"].weather["0"].icon + ".png";
+            document.getElementById('weatherIcon1').src = imagePath;
+            var windDay1 = parseFloat(forecast.list["0"].wind.speed * (18 / 5));
+            document.getElementById('windDay1').innerHTML = "Wind Speed: " + windDay1.toFixed(2) + "kmph";
+            document.getElementById('cloudsDay1').innerHTML = "Cloud Cover: " + forecast.list["0"].clouds.all + "%";
+            document.getElementById('humidityDay1').innerHTML = "Humidity: " + forecast.list["0"].main.humidity + "%";
+            var precipitationDay1 = forecast.list["0"].rain["3h"];
+            if (precipitationDay1 === undefined) {
+                precipitationDay1 = 0;
+            }
+            document.getElementById('rainDay1').innerHTML = "Precipitation: " + precipitationDay1.toFixed(2) + "mm";
 
-var conditionsPath = "http://api.openweathermap.org/data/2.5/weather?q=Kolkata,IN&units=metric&APPID=0dfa7081b88c8bb4ce043507ed280eb2&cnt=3";
-var forecastPath = "http://api.openweathermap.org/data/2.5/forecast?q=Kolkata,IN&units=metric&APPID=0dfa7081b88c8bb4ce043507ed280eb2&cnt=3";
+            // Day2
+            document.getElementById('dayDate2').innerHTML = aryDates[2];
+            document.getElementById('maxTemp2').innerHTML = forecast.list[1].main.temp_max + "&deg;C";
+            document.getElementById('minTemp2').innerHTML = forecast.list[1].main.temp_min + "&deg;C";
+            document.getElementById('dailyForecast2').innerHTML = forecast.list[1].weather["0"].description;
+            var imagePath = baseImgPath + forecast.list[1].weather["0"].icon + ".png";
+            document.getElementById('weatherIcon2').src = imagePath;
+            var windDay2 = parseFloat(forecast.list[1].wind.speed * (18 / 5));
+            document.getElementById('windDay2').innerHTML = "Wind Speed: " + windDay2.toFixed(2) + "kmph";
+            document.getElementById('cloudsDay2').innerHTML = "Cloud Cover: " + forecast.list[1].clouds.all + "%";
+            document.getElementById('humidityDay2').innerHTML = "Humidity: " + forecast.list[1].main.humidity + "%";
+            var precipitationDay2 = forecast.list[1].rain["3h"];
+            if (precipitationDay2 === undefined) {
+                precipitationDay2 = 0;
+            }
+            document.getElementById('rainDay2').innerHTML = "Precipitation: " + precipitationDay2.toFixed(2) + "mm";
 
-// GET THE CONDITIONS
-weatherConditions.open('GET', conditionsPath, true);
-weatherConditions.responseType = 'text';
-weatherConditions.send();
-
-// GET THE FORECARST
-weatherForecast.open('GET', forecastPath, true);
-weatherForecast.responseType = 'text';
-weatherForecast.send();
-
-weatherConditions.onload = function () {
-    if (weatherConditions.status === 200) {
-        conditionObj = JSON.parse(weatherConditions.responseText);
-        console.log(conditionObj);
-        document.getElementById('location').innerHTML = conditionObj.name + ", " + conditionObj.sys.country;
-        document.getElementById('weather').innerHTML = conditionObj.weather["0"].main;
-        var imagePath = baseImgPath + conditionObj.weather["0"].icon + ".png";
-        document.getElementById('weatherIcon').src = imagePath;
-        document.getElementById('temperature').innerHTML = conditionObj.main.temp + "&deg;C";
-        var weatherDescStr = conditionObj.weather["0"].description;
-        document.getElementById('desc').innerHTML = weatherDescStr.substr(0, 1).toUpperCase() + weatherDescStr.substr(1);
-        document.getElementById('humidity').innerHTML = "Humidity: " + conditionObj.main.humidity + "%";
-    }
-};
-
-var baseImgPath = 'http://openweathermap.org/img/w/';
-weatherForecast.onload = function () {
-    /*var baseImgPath = 'http://openweathermap.org/img/w/';*/
-    if (weatherForecast.status === 200) {
-        forecastObj = JSON.parse(weatherForecast.responseText);
-        console.log(forecastObj);
-
-        // Day1
-        document.getElementById('dayDate1').innerHTML = aryDates[1];
-        document.getElementById('maxTemp1').innerHTML = forecastObj.list["0"].main.temp_max + "&deg;C";
-        document.getElementById('minTemp1').innerHTML = forecastObj.list["0"].main.temp_min + "&deg;C";
-        var dailyWeatherDescStr1 = forecastObj.list["0"].weather["0"].description;
-        document.getElementById('dailyForecast1').innerHTML = dailyWeatherDescStr1.substr(0, 1).toUpperCase() + dailyWeatherDescStr1.substr(1);
-        var imagePath = baseImgPath + forecastObj.list["0"].weather["0"].icon + ".png";
-        document.getElementById('weatherIcon1').src = imagePath;
-
-        // Day2
-        document.getElementById('dayDate2').innerHTML = aryDates[2];
-        document.getElementById('maxTemp2').innerHTML = forecastObj.list[1].main.temp_max + "&deg;C";
-        document.getElementById('minTemp2').innerHTML = forecastObj.list[1].main.temp_min + "&deg;C";
-        var dailyWeatherDescStr2 = forecastObj.list[1].weather["0"].description
-        document.getElementById('dailyForecast2').innerHTML = dailyWeatherDescStr2.substr(0, 1).toUpperCase() + dailyWeatherDescStr2.substr(1);
-        var imagePath = baseImgPath + forecastObj.list[1].weather["0"].icon + ".png";
-        document.getElementById('weatherIcon2').src = imagePath;
-
-        // Day3
-        document.getElementById('dayDate3').innerHTML = aryDates[3];
-        document.getElementById('maxTemp3').innerHTML = forecastObj.list[2].main.temp_max + "&deg;C";
-        document.getElementById('minTemp3').innerHTML = forecastObj.list[2].main.temp_min + "&deg;C";
-        var dailyWeatherDescStr3 = forecastObj.list[2].weather["0"].description;
-        document.getElementById('dailyForecast3').innerHTML = dailyWeatherDescStr3.substr(0, 1).toUpperCase() + dailyWeatherDescStr3.substr(1);
-        var imagePath = baseImgPath + forecastObj.list[2].weather["0"].icon + ".png";
-        document.getElementById('weatherIcon3').src = imagePath;
-    }
-};
+            // Day3
+            document.getElementById('dayDate3').innerHTML = aryDates[3];
+            document.getElementById('maxTemp3').innerHTML = forecast.list[2].main.temp_max + "&deg;C";
+            document.getElementById('minTemp3').innerHTML = forecast.list[2].main.temp_min + "&deg;C";
+            document.getElementById('dailyForecast3').innerHTML = forecast.list[2].weather["0"].description;;
+            var imagePath = baseImgPath + forecast.list[2].weather["0"].icon + ".png";
+            document.getElementById('weatherIcon3').src = imagePath;
+            var windDay3 = parseFloat(forecast.list[2].wind.speed * (18 / 5));
+            document.getElementById('windDay3').innerHTML = "Wind Speed: " + windDay1.toFixed(2) + "kmph";
+            document.getElementById('cloudsDay3').innerHTML = "Cloud Cover: " + forecast.list[2].clouds.all + "%";
+            document.getElementById('humidityDay3').innerHTML = "Humidity: " + forecast.list[2].main.humidity + "%";
+            var precipitationDay3 = forecast.list[2].rain["3h"];
+            if (precipitationDay3 === undefined) {
+                precipitationDay3 = 0;
+            }
+            document.getElementById('rainDay3').innerHTML = "Precipitation: " + precipitationDay3.toFixed(2) + "mm";
+        }
+    });
+}
 
 function GetDates(startDate, daysToAdd) {
     var aryDates = [];
@@ -75,7 +85,7 @@ function GetDates(startDate, daysToAdd) {
     for (var i = 0; i <= daysToAdd; i++) {
         var currentDate = new Date();
         currentDate.setDate(startDate.getDate() + i);
-        aryDates.push(DayAsString(currentDate.getDay()) + ", " + currentDate.getDate() + " " + MonthAsString(currentDate.getMonth()) /*+ " " + currentDate.getFullYear()*/);
+        aryDates.push(DayAsString(currentDate.getDay()) + ", " + currentDate.getDate() + " " + MonthAsString(currentDate.getMonth()) /*+ " " + currentDate.getFullYear()*/ );
     }
 
     return aryDates;
